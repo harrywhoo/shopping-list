70143f1a1a59d8335fc2cb41f05f2199e728d0d4
harrywhoo <harryhu090@gmail.com>
2024-06-16 20:09:00 -0700

    Added functionality

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const itemForm = document.querySelector("#item-form")
 const itemInput = document.querySelector("#item-input")
 const itemList = document.querySelector("#item-list")
+const clearButton = document.getElementById("clear")
+const filter = document.getElementById("filter")
 
 
-itemForm.addEventListener('submit', (e)=>{
+function checkUI() {
+    const items = itemList.querySelectorAll('li')
+    if (items.length === 0) {
+        clearButton.style.display = 'none'
+        filter.style.display = 'none'
+    }
+    else {
+        clearButton.style.display = 'block'
+        filter.style.display = 'block'
+    }
+}
+
+itemForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const newItem = itemInput.value
-    if (newItem === ''){
+    if (newItem === '') {
         alert('Please add an item')
         return
     }
+    addItemToDOM(newItem)
+    addItemToStorage(newItem)
 
+    itemInput.value = ''
+    checkUI()
+})
+
+function addItemToDOM(item) {
     const li = document.createElement("li")
-    const txt = document.createTextNode(newItem);
+    const txt = document.createTextNode(item);
     li.appendChild(txt);
 
     const button = document.createElement('button')
@@ -25,8 +46,49 @@ itemForm.addEventListener('submit', (e)=>{
 
     li.appendChild(button)
 
-    console.log(li)
     itemList.appendChild(li);
+}
 
-    itemInput.value = ''
+function addItemToStorage(item) {
+    let itemsFromStorage
+
+    if (localStorage.getItem('items') === null) {
+        itemsFromStorage = []
+
+    } else {
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+    }
+    itemsFromStorage.push(item)
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+itemList.addEventListener('click', (e) => {
+    if (e.target.parentElement.classList.contains("remove-item")) {
+        if (confirm("Are you sure?")) {
+            e.target.parentElement.parentElement.remove()
+        }
+    }
+    checkUI()
 })
+
+clearButton.addEventListener('click', (e) => {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+    checkUI()
+})
+
+filter.addEventListener('input', (e) => {
+    const text = e.target.value.toLowerCase()
+    const items = itemList.querySelectorAll('li')
+    items.forEach((item) => {
+        const itemName = item.firstChild.textContent.toLowerCase()
+        if (!itemName.includes(text)) {
+            item.style.display = 'none'
+        } else {
+            item.style.display = 'flex'
+        }
+    })
+})
+
+checkUI()
