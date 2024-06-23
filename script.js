68e20e03b98b8c2f8e68fe44d1ac92094cb5cc5f
harrywhoo <harryhu090@gmail.com>
2024-06-22 18:52:33 -0700

    Finished project

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const itemInput = document.querySelector("#item-input")
 const itemList = document.querySelector("#item-list")
 const clearButton = document.getElementById("clear")
 const filter = document.getElementById("filter")
+const formBtn = document.querySelector('button')
+let isEditMode = false
 
-
+function displayItems() {
+    const itemsFromStorage = getItemsFromStorage()
+    itemsFromStorage.forEach((item) => addItemToDOM(item))
+}
 function checkUI() {
     const items = itemList.querySelectorAll('li')
+    if (isEditMode) {
+        filter.style.display = 'none'
+    }
     if (items.length === 0) {
         clearButton.style.display = 'none'
         filter.style.display = 'none'
@@ -16,22 +24,67 @@ function checkUI() {
         filter.style.display = 'block'
     }
 }
+window.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON' ||
+        e.target.tagName === 'LI' ||
+        e.target.id === 'item-input') {
+        return
+    }
+    if (isEditMode) {
+        formBtn.innerHTML = `<i class="fa-solid fa-plus"></i>
+                Add Item`
+        formBtn.style.backgroundColor = "#333"
+        itemInput.value = ''
+        itemList.querySelectorAll("li").forEach((i) => {
+            i.classList.remove('edit-mode')
+        })
+        isEditMode = false
+    }
 
+})
 itemForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const newItem = itemInput.value
     if (newItem === '') {
-        alert('Please add an item')
+        swal.fire({
+            text: "Empty Item",
+            icon: "error",
+            background: "#f5f5f5",
+            confirmButtonColor: "#228B22",
+        });
         return
+    }
+    if (isEditMode) {
+        const currItem = itemList.querySelector(".edit-mode")
+        deleteItemFromStorage(currItem.textContent)
+        itemList.removeChild(currItem)
+        isEditMode = false
+    }
+    else {
+        if (checkItemExists(newItem)) {
+            swal.fire({
+                text: "Item already exists",
+                icon: "error",
+                background: "#f5f5f5",
+                confirmButtonColor: "#228B22",
+            });
+            return
+        }
     }
     addItemToDOM(newItem)
     addItemToStorage(newItem)
-
+    formBtn.innerHTML = `<i class="fa-solid fa-plus"></i>
+    Add Item`
+    formBtn.style.backgroundColor = "#333"
     itemInput.value = ''
-    checkUI()
+
 })
 
+function checkItemExists(item) {
+    let itemsFromStorage = getItemsFromStorage();
+    return itemsFromStorage.includes(item)
+}
 function addItemToDOM(item) {
     const li = document.createElement("li")
     const txt = document.createTextNode(item);
@@ -47,38 +100,93 @@ function addItemToDOM(item) {
     li.appendChild(button)
 
     itemList.appendChild(li);
+    checkUI()
+}
+function deleteItemFromStorage(item) {
+    let itemsFromStorage = getItemsFromStorage();
+    if (!itemsFromStorage.includes(item)) {
+        return
+    }
+    itemsFromStorage = itemsFromStorage.filter((i) => i !== item)
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+function addItemToStorage(item) {
+    const itemsFromStorage = getItemsFromStorage()
+    itemsFromStorage.push(item)
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
 }
 
-function addItemToStorage(item) {
-    let itemsFromStorage
-
+function getItemsFromStorage() {
     if (localStorage.getItem('items') === null) {
         itemsFromStorage = []
 
     } else {
         itemsFromStorage = JSON.parse(localStorage.getItem('items'))
     }
-    itemsFromStorage.push(item)
-    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+    return itemsFromStorage
 }
-
 itemList.addEventListener('click', (e) => {
     if (e.target.parentElement.classList.contains("remove-item")) {
-        if (confirm("Are you sure?")) {
-            e.target.parentElement.parentElement.remove()
-        }
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#228B22",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+                deleteItemFromStorage(e.target
+                    .parentElement
+                    .parentElement
+                    .textContent)
+                e.target.parentElement.parentElement.remove()
+            }
+            checkUI()
+        });
+    }
+    else if (e.target.tagName === 'LI') {
+        setItemToEdit(e.target)
     }
     checkUI()
 })
 
+function setItemToEdit(item) {
+    isEditMode = true
+    itemList.querySelectorAll("li").forEach((i) => {
+        i.classList.remove('edit-mode')
+    })
+    item.classList.add('edit-mode')
+    formBtn.innerHTML = `<i class="fa-solid fa-pen"></i>
+    Update Item`
+    formBtn.style.backgroundColor = "#228B22"
+    itemInput.value = item.textContent
+    checkUI()
+}
 clearButton.addEventListener('click', (e) => {
+    if (isEditMode) {
+        swal.fire({
+            text: "Finish edit first",
+            background: "#f5f5f5",
+            confirmButtonColor: "#228B22",
+        });
+        return
+    }
     while (itemList.firstChild) {
+        deleteItemFromStorage(itemList.firstChild.textContent)
         itemList.removeChild(itemList.firstChild)
     }
     checkUI()
 })
 
 filter.addEventListener('input', (e) => {
+
     const text = e.target.value.toLowerCase()
     const items = itemList.querySelectorAll('li')
     items.forEach((item) => {
@@ -91,4 +199,5 @@ filter.addEventListener('input', (e) => {
     })
 })
 
+document.addEventListener('DOMContentLoaded', displayItems)
 checkUI()
